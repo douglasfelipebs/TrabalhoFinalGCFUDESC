e5fe67cac70052e8d2e23842285b0c7c7a780c32
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2018-11-03 01:03:12 -0300

    Script para criação da tabela de cliente #3

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
---SCRIPTS BANCO LOJA INFORMÁTICA:
+﻿--SCRIPTS BANCO LOJA INFORMÁTICA:
 
 --CRIANDO TABELA DE PRODUTOS
 create table if not exists tbproduto (
@@ -17,3 +17,20 @@ WITH series AS (SELECT generate_series(1, (SELECT max(procodigo)
 insert into tbproduto VALUES (SELECT COALESCE((SELECT min(mysequence) FROM series WHERE series.mysequence NOT IN(SELECT procodigo FROM tbproduto)), (SELECT max(procodigo) + 1          FROM tbproduto), 1), 'Notebook', 'Notebook ACER 16GB RAM/Intel Core i7 8th Gen/1TB SDD', 'ACER', 5000.00);
 insert into tbproduto VALUES (SELECT COALESCE((SELECT min(mysequence) FROM series WHERE series.mysequence NOT IN(SELECT procodigo FROM tbproduto)), (SELECT max(procodigo) + 1          FROM tbproduto), 1), 'Notebook', 'Notebook DELL 16GB RAM/Intel Core i7 8th Gen/1TB SDD', 'DELL', 5000.00);
 insert into tbproduto VALUES (SELECT COALESCE((SELECT min(mysequence) FROM series WHERE series.mysequence NOT IN(SELECT procodigo FROM tbproduto)), (SELECT max(procodigo) + 1          FROM tbproduto), 1), 'Notebook', 'Notebook SAMSUNG 16GB RAM/Intel Core i7 8th Gen/1TB SDD', 'SAMSUNG', 3000.00);
+
+
+/* Cliente  */
+CREATE TABLE IF NOT EXISTS TBCLIENTE (
+	CLICODIGO INTEGER NOT NULL,
+	CLINOME CHARACTER VARYING(50) NOT NULL,
+	CLICPF CHARACTER VARYING(14) NOT NULL,
+	CLILOGRADOURO CHARACTER VARYING(200) NOT NULL,
+	CLINUMERO BIGINT NOT NULL,
+	CLIBAIRRO CHARACTER VARYING(80) NOT NULL,
+	CLICIDADE CHARACTER VARYING(80) NOT NULL,
+	CLICEP CHARACTER VARYING(9) NOT NULL,
+	CLIUF CHARACTER VARYING(2) NOT NULL
+);
+
+ALTER TABLE TBCLIENTE DROP CONSTRAINT IF EXISTS tbcliente_pkey;
+ALTER TABLE TBCLIENTE ADD PRIMARY KEY (CLICODIGO)
